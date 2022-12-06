283135b56c4eb3f735b3dc04cd000d82cd648c81
poojanandakumar <poojanandakumar34@gmail.com>
2022-12-06 23:16:45 +0530

    Day 2
    learn to reverse a string

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,32 @@
-fun main(){
-    println("pooja")
+fun main() {
+//    reverseArray()
+    reverseString()
+}
+
+fun reverseArray(){
+    val array = arrayListOf(2,4,6,8,10,7,9,1)
+    var i = 0
+    var j = array.size-1
+    while (i<(array.size-1)/2){
+        val temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+        i += 1
+        j -= 1
+    }
+    println(array)
+}
+
+fun reverseString(){
+    val string = "pooja"
+    var result = ""
+    var i = string.length-1
+    println(i)
+
+    while (i>0){
+        result += string.get(i).toString()
+        i -= 1
+    }
+    println(result)
+    //or string.reversed
 }
