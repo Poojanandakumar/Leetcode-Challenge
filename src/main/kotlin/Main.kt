88b38c74d5632758a965cd980e650e8866198f73
poojanandakumar <poojanandakumar34@gmail.com>
2022-12-07 20:44:34 +0530

    Day 3
    Implement function to find min and max of a given array.
    Implement function to find missing number of a given array.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
 fun main() {
+    val array = arrayListOf(2,4,6,8,100,7,9,1)
 //    reverseArray()
 //    reverseString()
-    searchAnElement()
+//    searchAnElement()
+//    maxAndMin(array)
+    missnNum()
 }
 
-fun reverseArray(){
-    val array = arrayListOf(2,4,6,8,10,7,9,1)
+fun reverseArray(array:ArrayList<Int>){
     var i = 0
     var j = array.size-1
     while (i<(array.size-1)/2){
@@ -18,7 +20,7 @@ fun reverseArray(){
     println(array)
 }
 
-fun reverseString(){
+fun reverseString(array:ArrayList<Int>){
     val string = "pooja"
     var result = ""
     var i = string.length-1
@@ -31,13 +33,43 @@ fun reverseString(){
     //or string.reversed
 }
 
-fun searchAnElement(){
-    val array = arrayListOf(2,4,6,8,10,7,9,1)
+fun searchAnElement(array:ArrayList<Int>){
     var i = 0
     val textElement = 10
     var result = 0
     while (i<array.size-1){
         if (array[i]==textElement){
+            result = i
+        }
+        i+=1
+    }
+    println(result)
+}
+
+fun maxAndMin(array:ArrayList<Int>){
+
+    var min = array[0]
+    var max = array[0]
+    var i = 0
+    while (i<array.size){
+        if(array[i]<min){
+            min = array[i]
+        }
+        if(array[i]>max){
+            max = array[i]
+        }
+        i+=1
+    }
+    println(min)
+    println(max)
+}
+
+fun missnNum(){
+    val a = arrayListOf(1,2,3,4,5,7,8)
+    var i = 1
+    var result = 0
+    while (i<=a.size){
+        if(!a.contains(i)){
             result = i
         }
         i+=1
